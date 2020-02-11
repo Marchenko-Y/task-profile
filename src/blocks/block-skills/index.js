4c83c0b1398602b277084062bb7f51a3ae6f4954
marchenko ulya <marchenko.ulya26@gmail.com>
2020-02-11 09:50:13 +0300

    Added block-skills

---
+++ src/blocks/block-skills/index.js
@@ -0,0 +1,1 @@
+import './block-skills.scss';
--- src/blocks/index.js
+++ src/blocks/index.js
@@ -1,5 +1,6 @@
 import './block-example';
 import './block-profile';
+import './block-skills';
 
 export default () => {
   console.log('init blocks');
